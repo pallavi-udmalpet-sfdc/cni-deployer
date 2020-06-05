1e94557e51e60447277b007d48e2d273d70bc879
Kevin Wang <kevinwang@salesforce.com>
2020-06-05 15:49:17 -0700

    remove casting to string since print already does this implicitly

--- Infrastructure/deployer/deployment_helper.py
+++ Infrastructure/deployer/deployment_helper.py
@@ -13,7 +13,7 @@ def export_to_environment(manifest_file_path):
 
 def get_outbound_vpc_suffixes(manifest_file_path):
     manifest = common.load_yaml(manifest_file_path)
-    for vpc_suffix in [str(vpc_suffix) for vpc_suffix in manifest['outbound_vpcs_config'].keys()]:
+    for vpc_suffix in manifest['outbound_vpcs_config'].keys():
         print(vpc_suffix)
 
 
